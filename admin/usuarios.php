189479b2a037d823b27cd8647578c403979c5ecf
Vinicius01br <viniciuslima1809@hotmail.com>
2024-05-29 17:24:58 -0300

    microblog php admin

--- admin/usuarios.php
+++ admin/usuarios.php
@@ -1,23 +1,25 @@
-<?php 
+<?php
 require_once "../inc/cabecalho-admin.php";
+require_once "../inc/funcoes-usuarios.php";
+$listaDeUsuarios = lerUsuarios($conexao);
 ?>
-
+<pre><?= var_dump($listaDeUsuarios) ?></pre>
 
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
-		
+
 		<h2 class="text-center">
-		Usuários <span class="badge bg-dark">X</span>
+			Usuários <span class="badge bg-dark">X</span>
 		</h2>
 
 		<p class="text-center mt-5">
 			<a class="btn btn-primary" href="usuario-insere.php">
-			<i class="bi bi-plus-circle"></i>	
-			Inserir novo usuário</a>
+				<i class="bi bi-plus-circle"></i>
+				Inserir novo usuário</a>
 		</p>
-				
+
 		<div class="table-responsive">
-		
+
 			<table class="table table-hover">
 				<thead class="table-light">
 					<tr>
@@ -35,27 +37,24 @@ require_once "../inc/cabecalho-admin.php";
 						<td> E-mail... </td>
 						<td> Tipo... </td>
 						<td class="text-center">
-							<a class="btn btn-warning" 
-							href="usuario-atualiza.php">
-							<i class="bi bi-pencil"></i> Atualizar
+							<a class="btn btn-warning" href="usuario-atualiza.php">
+								<i class="bi bi-pencil"></i> Atualizar
 							</a>
-						
-							<a class="btn btn-danger excluir" 
-							href="usuario-exclui.php">
-							<i class="bi bi-trash"></i> Excluir
+
+							<a class="btn btn-danger excluir" href="usuario-exclui.php">
+								<i class="bi bi-trash"></i> Excluir
 							</a>
 						</td>
 					</tr>
 
-				</tbody>                
+				</tbody>
 			</table>
-	</div>
-		
+		</div>
+
 	</article>
 </div>
 
 
-<?php 
+<?php
 require_once "../inc/rodape-admin.php";
 ?>
-
--- inc/funcoes-usuarios.php
+++ inc/funcoes-usuarios.php
@@ -2,9 +2,14 @@
 
 /*Acessando os dados da conexão ao servidor */
 require "conecta.php";
-function inserirUsuario($conexao, $nome, $email, $tipo, $senha){
- $sql =  "INSERT INTO usuarios(nome, email, tipo, senha) VALUES('$nome', '$email', '$tipo', '$senha')";
-    mysqli_query($conexao, $sql) or die (mysqli_error($conexao));
+function inserirUsuario($conexao, $nome, $email, $tipo, $senha)
+{
+    $sql =  "INSERT INTO usuarios(nome, email, tipo, senha) VALUES('$nome', '$email', '$tipo', '$senha')";
+    mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
 }
+function lerUsuarios($conexao){
+    $sql = "SELECT id, nome tipo, email FROM usuarios";
+     $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao)) or die(mysqli_error($conexao));
 
-?>
+     return mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+}
